72cf51bc7bb00d7937fbc5cd056c3ee454d58cdc
Nitya <knitya23@gmail.com>
2021-10-27 11:25:09 +0530

    first commit

---
+++ src/app/note-card/note-card.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit, ViewChild, ElementRef, Renderer2, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-note-card',
+  templateUrl: './note-card.component.html',
+  styleUrls: ['./note-card.component.scss']
+})
+export class NoteCardComponent implements OnInit {
+  
+  @Input() 
+  title!: string;
+  @Input()
+  body!: string;
+  @ViewChild('truncator') 
+  truncator!: ElementRef<HTMLElement>;
+  @ViewChild('bodyText')
+  bodyText!: ElementRef<HTMLElement>;
+  
+  constructor(private renderer: Renderer2) {
+  
+   }
+
+  ngOnInit(): void {
+
+  }
+
+  ngAfterViewInit(): void {
+    let style = window.getComputedStyle(this.bodyText.nativeElement, null);
+    let viewableHeight = parseInt(style.getPropertyValue("height"), 10);
+  
+    if (this.bodyText.nativeElement.scrollHeight > viewableHeight) {
+      this.renderer.setStyle(this.truncator.nativeElement, 'display', 'block');
+    } else {
+      this.renderer.setStyle(this.truncator.nativeElement, 'display', 'none');
+    }
+  }
+
+
+
+
+}
